aedab01f3da85d9e2a854eac10ecbf0bc7abdb39
Dima4987 <125592524+Dima4987@users.noreply.github.com>
2023-02-17 20:38:16 +0300

    NPC code

---
+++ NPC.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC: MonoBehaviour
+{   //Здоровье нпс
+    public int health = 5;
+    
+
+    //Уровень нпс
+    public int level = 1;
+
+    //Скорость нпс
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print("Жизни: " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z +=speed * Time.deltaTime;
+        transform.position = newPosition;
+
+    }
+}
